3c92dde61ca482c5d7a73bf03ab384cd79f0812a
Santamanches <santamanches@gmail.com>
2022-12-22 20:42:26 +0300

    save

---
+++ main.js
@@ -0,0 +1,29 @@
+
+
+
+var str = prompt('Введи свое имя:')
+
+
+
+
+var ch1 = (str)
+
+var ch1 = +prompt('введи любое число')
+
+
+var ch2 = (str)
+
+var ch2 = +prompt('введи любое число')
+
+var ch3 = (str)
+
+var ch3 = +prompt('введи любое число')
+
+let Averagenumber = [ ch1, ch2, ch3 ];
+
+alert( 'Среднее арифметическое чисел равно: ' + (ch1+ch2+ch3 )/3 )
+
+
+
+
+
